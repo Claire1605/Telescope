0c0b79483da79dc1bf4730117c31904f0b35567b
Claire1605 <claire1605@gmail.com>
2018-06-04 23:04:10 +0100

    Add initial unity project with progress from Ferry crossing <3

---
+++ Assets/ZoomZone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomZone : MonoBehaviour
+{
+    public float maxZoomScale;
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
